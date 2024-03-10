f762eb05e288d2facbd5f0a57cc2ae2af88cf7b2
dorrlixD1 <dorr.dominik@protonmail.com>
2024-03-10 18:28:23 +0100

    Init

---
+++ src/main/java/at/technikum/reslience/ReslienceApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.reslience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReslienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReslienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/technikum/reslience/ReslienceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.reslience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReslienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
